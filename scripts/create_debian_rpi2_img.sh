157f5afeceecaff6896d773d7bd6b1219005fea8
Andreas Florath <andreas@florath.net>
2015-09-28 08:25:03 +0200

    Add encryption and chroot script handling

--- scripts/create_debian_rpi2_img.sh
+++ scripts/create_debian_rpi2_img.sh
@@ -8,15 +8,19 @@ set -e
 function usage() {
     test -n "$1" && echo "*** ERROR: $1" >&2
     cat <<EOF >&2
-Usage: create_debian_rpi_img -w working_dir -d distribution -v variant -s image_size [-p proxy]
+Usage: create_debian_rpi_img -w working_dir -d distribution -v variant -s image_size
+                             -e enc_disk_id [-p pkglist] [-P proxy] [-c chroot_sh]
 where
   working_dir  is the place where the image is build
                Some gigs of HD space should be available there.
   distribution one of debian or ubuntu
   variant      the version, like jessie, stretch or wily
   image_size   the initial image size, e.g. '1G'
+  enc_disk_id  disk id of the USB stick where the decryption key is stored
+  pkglist      [optional] comma separated list of additional packages
   proxy        [optional] when there is the need to set the http(s)_proxy
-               set this to the appropriate url.
+               set this to the appropriate url
+  chroot_sh    [optional] Script that is executed in chroot
 EOF
     exit 1
 }
@@ -26,8 +30,11 @@ DISTRIBUTION=""
 VARIANT=""
 IMAGE_SIZE=""
 PROXY=""
+ENC_DISK_ID=""
+ADD_PACKAGES=""
+USER_CHROOT_SH=""
 
-while getopts "hw:d:v:s:p:" opt;
+while getopts "hw:d:v:s:P:e:p:c:" opt;
 do
     case ${opt} in
 	h)
@@ -35,6 +42,9 @@ do
 	    ;;
 	w)
 	    WORKING_DIR=${OPTARG}
+	    ;;
+	c)
+	    USER_CHROOT_SH=${OPTARG}
 	    ;;
 	d)
 	    DISTRIBUTION=${OPTARG}
@@ -46,7 +56,13 @@ do
 	    IMAGE_SIZE=${OPTARG}
 	    ;;
 	p)
+	    ADD_PACKAGES=${OPTARG}
+	    ;;
+	P)
 	    PROXY=${OPTARG}
+	    ;;
+	e)
+	    ENC_DISK_ID=${OPTARG}
 	    ;;
 	\?)
 	    usage "Invalid oprtion [${opt}]"
@@ -61,6 +77,7 @@ test -z "${WORKING_DIR}" && usage "working dir [-w] not set"
 test -z "${DISTRIBUTION}" && usage "distribution [-d] not set"
 test -z "${VARIANT}" && usage "variant [-v] not set"
 test -z "${IMAGE_SIZE}" && usage "image size [-v] not set"
+test -z "${ENC_DISK_ID}" && usage "encrypted disk id [-e] not set"
 
 if test -n "${PROXY}";
 then
@@ -74,13 +91,18 @@ mkdir -p ${WORKING_DIR}
 # This is the place where the new image will be bootstraped.
 CROOT=${WORKING_DIR}/root
 mkdir -p ${CROOT}
-CROOT_FW=${WORKING_DIR}/root/boot/firmware
+CROOT_FW=${CROOT}/boot/firmware
+CROOT_ENC=${CROOT}/enc
 
 # If something nasty happens: cleanup
 function cleanup() {
+    umount ${CROOT}/proc || true
+    umount ${CROOT}/sys || true
     umount ${CROOT_FW} || true
+    umount ${CROOT_ENC} || true
     umount ${CROOT} || true
-    lvchange -an /dev/rpi2vg/root_vol || true
+    lvchange -an /dev/rpi2vg/*_vol || true
+    cryptsetup close /dev/mapper/crypteddisk || true
     test ! -z "${LOOPDEV}" && kpartx -d "${LOOPDEV}" || true
     test ! -z "${LOOPDEV}" && losetup --detach ${LOOPDEV}
     rm -fr ${CROOT}
@@ -101,42 +123,86 @@ parted -s ${LOOPDEV} "mklabel msdos"
 # Create /boot/firmware
 parted -s ${LOOPDEV} "unit s" "mkpart primary fat16 2048 249855"
 # Create /root
-parted -s ${LOOPDEV} "unit s" "mkpart primary ext4 249856 -1"
+parted -s ${LOOPDEV} "unit s" "mkpart primary ext4 249856 2347007"
+# Create /enc
+parted -s ${LOOPDEV} "unit s" "mkpart primary ext4 2347008 -1"
 # Make the partitions available for the system
 kpartx -a ${LOOPDEV}
 
 # Typically the kernel need some time to get the partitions up
 # and running.
 LOOPDEVBASE=$(echo ${LOOPDEV} | cut -d "/" -f 3)
-while test ! -e /dev/mapper/${LOOPDEVBASE}p1;
+for p in 1 2 3;
 do
-    sleep 0.2
+    while test ! -e /dev/mapper/${LOOPDEVBASE}p${p};
+    do
+	sleep 0.2
+    done
 done
+
+# Create the key
+# Create some random numbers
+test ! -e ${WORKING_DIR}/rpi2-usb-random.key && \
+    dd if=/dev/urandom of=${WORKING_DIR}/rpi2-usb-random.key bs=512 count=60
+chmod 0400 ${WORKING_DIR}/rpi2-usb-random.key
+# Extract the key
+dd if=${WORKING_DIR}/rpi2-usb-random.key \
+   of=${WORKING_DIR}/rpi2-enc.key bs=512 count=8
+
+echo "*** PLEASE copy over the random keys to the USB stick, e.g.:"
+echo "*** dd if=${WORKING_DIR}/rpi2-usb-random.key seek=1 of=/dev/disk/by-id/${ENC_DISK_ID}"
+
+# Format /
+LOOPDEVROOT=/dev/mapper/${LOOPDEVBASE}p2
+mkfs.ext4 ${LOOPDEVROOT}
+mount ${LOOPDEVROOT} ${CROOT}
+
+# The rest goes to encrypted LVM (enc)
+LOOPDEVENC=/dev/mapper/${LOOPDEVBASE}p3
+
+cryptsetup luksFormat --batch-mode --cipher=aes-xts-plain64 --hash=sha512 \
+	   --key-size=512 \
+	   ${LOOPDEVENC} ${WORKING_DIR}/rpi2-enc.key
+
+cryptsetup luksOpen \
+	   --key-file ${WORKING_DIR}/rpi2-enc.key \
+	   ${LOOPDEVENC} crypteddisk
+
+ENCDISK=/dev/mapper/crypteddisk
+
+pvcreate ${ENCDISK}
+vgcreate rpi2vg ${ENCDISK}
+lvcreate -l 100%FREE -n enc_vol rpi2vg
+mkfs.ext4 /dev/rpi2vg/enc_vol
+mkdir -p ${CROOT_ENC}
+mount /dev/rpi2vg/enc_vol ${CROOT_ENC}
 
 # Format /boot/firmware
 mkfs.fat /dev/mapper/${LOOPDEVBASE}p1
 mkdir -p ${WORKING_DIR}/root/boot/firmware
-
-# The rest goes to LVM
-LOOPDEVROOT=/dev/mapper/${LOOPDEVBASE}p2
-pvcreate ${LOOPDEVROOT}
-vgcreate rpi2vg ${LOOPDEVROOT}
-
-lvcreate -l 100%FREE -n root_vol rpi2vg
-mkfs.ext4 /dev/rpi2vg/root_vol
-mount /dev/rpi2vg/root_vol ${CROOT}
 mkdir -p ${CROOT_FW}
 mount /dev/mapper/${LOOPDEVBASE}p1 ${CROOT_FW}
 
+# Use /enc for /home and some system things
+mkdir -p ${CROOT_ENC}/home
+
+mkdir -p ${CROOT_ENC}/system
+
+
+PACKAGES="lvm2,apt-transport-https,wget,openssl,ca-certificates,"
+PACKAGES+="apt-utils,net-tools,iproute2,cryptsetup-bin"
+
+test -n "${ADD_PACKAGES}" && PACKAGES+=",${ADD_PACKAGES}"
+
 debootstrap --arch=armhf \
-	    --include=lvm2,apt-transport-https,wget,openssl,ca-certificates \
+	    --include=${PACKAGES} \
 	    --components=main,contrib,non-free \
 	    --variant=minbase --verbose --foreign \
 	    ${VARIANT} \
 	    ${CROOT} \
 	    http://httpredir.debian.org/debian
 
-cp /usr/bin/qemu-arm-static  ${CROOT}/usr/bin
+cp /usr/bin/qemu-arm-static ${CROOT}/usr/bin
 
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true \
 	       LC_ALL=C LANGUAGE=C LANG=C chroot ${CROOT} \
@@ -157,6 +223,7 @@ cat <<EOF >${CROOT}/chroot_cmd.sh
 #!/bin/bash
 
 set -e
+set -x
 
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
@@ -180,6 +247,9 @@ fi
 mount -t proc none /proc
 mount -t sysfs none /sys
 
+echo "exit 101" >/usr/sbin/policy-rc.d
+chmod a+x /usr/sbin/policy-rc.d
+
 # 'true' needed because key not available at this point of time
 apt-get --yes update || true 
 apt-get --force-yes --yes install collabora-obs-archive-keyring || true
@@ -190,8 +260,12 @@ apt-get --yes install linux-image-3.18.0-trunk-rpi2
 apt-get --yes install raspberrypi-bootloader-nokernel
 apt-get --yes install flash-kernel
 
+mv /home /enc
+ln -sf /enc/home /home
+
 useradd --create-home dummy
 chpasswd <<LEOF
+root:qwe
 dummy:qwe
 LEOF
 
@@ -213,6 +287,7 @@ cat <<EOF >${CROOT}/etc/fstab
 proc            /proc           proc    defaults          0       0
 /dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
 /dev/mmcblk0p1  /boot/firmware  vfat    defaults          0       2
+/dev/rpi2vg/enc_vol  /enc       ext4    defaults          0       2
 EOF
 
 echo debianrpi2 >${CROOT}/etc/hostname
@@ -244,53 +319,13 @@ cat <<EOF >${CROOT}/boot/firmware/config.txt
 # http://www.raspberrypi.org/documentation/configuration/config-txt.md
 # Some settings may impact device functionality. See link above for details
 
-# uncomment if you get no picture on HDMI for a default "safe" mode
-#hdmi_safe=1
-
-# uncomment this if your display has a black border of unused pixels visible
-# and your display can output without overscan
-#disable_overscan=1
-
-# uncomment the following to adjust overscan. Use positive numbers if console
-# goes off screen, and negative if there is too much border
-#overscan_left=16
-#overscan_right=16
-#overscan_top=16
-#overscan_bottom=16
-
-# uncomment to force a console size. By default it will be display's size minus
-# overscan.
-#framebuffer_width=1280
-#framebuffer_height=720
-
-# uncomment if hdmi display is not detected and composite is being output
-#hdmi_force_hotplug=1
-
-# uncomment to force a specific HDMI mode (this will force VGA)
-#hdmi_group=1
-#hdmi_mode=1
-
-# uncomment to force a HDMI mode rather than DVI. This can make audio work in
-# DMT (computer monitor) modes
-#hdmi_drive=2
-
-# uncomment to increase signal to HDMI, if you have interference, blanking, or
-# no display
-#config_hdmi_boost=4
-
-# uncomment for composite PAL
-#sdtv_mode=2
-
-#uncomment to overclock the arm. 700 MHz is the default.
-#arm_freq=800
 EOF
 
-echo 'dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootwait' > ${CROOT}/boot/firmware/cmdline.txt
+echo 'dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 elevator=deadline rootfstype=ext4 rootwait' > ${CROOT}/boot/firmware/cmdline.txt
 
 # XXX There are two links created - I have no idea why and whereto
 # ln -sf ${CROOT}/... config
 # ln -sf ${CROOT}/... cmdline
-
 
 # Load sound module on boot
 mkdir -p ${CROOT}/lib/modules-load.d
@@ -306,6 +341,16 @@ blacklist snd_soc_pcm512x
 blacklist snd_soc_tas5713
 blacklist snd_soc_wm8804
 EOF
+
+cat <<EOF >${CROOT}/etc/crypttab
+# <target name> <source device>         <key file>      <options>
+lvm /dev/mmcblk0p3 /dev/disk/by-id/${ENC_DISK_ID} luks,tries=3,keyfile-size=4096,keyfile-offset=512
+EOF
+
+test -n "${USER_CHROOT_SH}" \
+    && cp "${USER_CHROOT_SH}" ${CROOT}/user_chroot.sh \
+    && cr /bin/bash -x -e /user_chroot.sh \
+    && rm ${CROOT}/user_chroot.sh
 
 echo "START"
 /bin/bash
