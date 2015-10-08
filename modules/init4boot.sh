cd207fd8badc761b23cb6877892ea1c5fbb0467a
Andreas Florath <andreas@florath.net>
2015-10-08 07:01:52 +0200

    Added initrd creation step to init4boot module

--- modules/init4boot.sh
+++ modules/init4boot.sh
@@ -4,7 +4,24 @@
 
 CMD=$1
 
-function initrd_install() {
+function init4boot_prepare_contrib() {
+    cd ${CONTRIB_DIR}
+    if test -e init4boot; then
+	(cd init4boot && git pull)
+    else
+	git clone https://github.com/florath/init4boot.git
+    fi
+}
+
+function init4boot_initrd_create() {
+    KERNEL_DESC=$( (cd ${CROOT}/lib/modules && echo *) )
+
+    ${CONTRIB_DIR}/init4boot/i4b-mkinitramfs \
+		  --output= ${CROOT}/boot/initrd.img-${KERNEL_DESC} \
+		  --root-dir=${CROOT} --plugins=${CONTRIB_DIR}/init4boot
+}
+
+function init4boot_initrd_install() {
     INITRD="$(ls -1 ${CROOT}/boot/initrd.img-* | sort | tail -n 1)"
     test -z "${INITRD}" && exit 1
     mkimage -A arm -O linux -T ramdisk -C gzip -a 0x00000000 -e 0x00000000 \
@@ -17,7 +34,13 @@ case ${CMD} in
 	PACKAGES+="busybox,open-iscsi,aufs-tools,mdadm,multipath-tools,"
 	PACKAGES+="kpartx,klibc-utils,"
 	;;
+    prepare_contrib)
+	init4boot_prepare_contrib
+	;;
+    initrd_create)
+	init4boot_initrd_create
+	;;
     initrd_install)
-	initrd_install
+	init4boot_initrd_install
 	;;
 esac
